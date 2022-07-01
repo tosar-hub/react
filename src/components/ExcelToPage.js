fbb6b43e318e07b678741e8f7dc695fa7addb523
Anatoliy <tosar@yandex.ru>
2022-07-01 17:18:48 -0400

    fix 1

--- src/components/ExcelToPage.js
+++ src/components/ExcelToPage.js
@@ -4,21 +4,32 @@ import {JsonToTable} from "react-json-to-table";
 import '../styles/ExcelToPage.css'
 
 function ExcelToPage() {
-    const [columns, setColumns] = useState([]);
+    const [columns, setColumns] = useState(null);
+    const [file, setFile] = useState('');
     const onChange = async (e) => {
         const file = e.target.files[0];
+        const goodName = ['xls', 'xlsx']
+        const checkName = fil => {
+            return goodName.includes(fil.name.split(".")[1]);
+        }
+        if (!checkName(file)) {
+            alert('invalid file type !!!')
+            setColumns(null)
+            setFile('')
+            return;
+        }
         const data = await file.arrayBuffer();
         const workbook = XLSX.readFile(data);
         const worksheet = workbook.Sheets[workbook.SheetNames[0]];
         const jsonData = XLSX.utils.sheet_to_json(worksheet);
         setColumns(jsonData);
-
-        // console.log(columns);
     }
     return (
         <div className='ExcelToPage'>
-            <input type="file" onChange={onChange}/>
-            <JsonToTable json={columns}/>
+            <input value={file} type="file" onChange={onChange}/>
+            <JsonToTable
+                key={Date.now()}
+                json={columns}/>
         </div>
     )
 }
